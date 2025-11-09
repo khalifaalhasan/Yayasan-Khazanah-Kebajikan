d89f900eae00338e3df69d335e258d05ec3c322e
NakamaShp <khlfaalhsn5@gmail.com>
2025-11-10 02:44:35 +0700

    fixing production

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,7 +1,12 @@
 import { createServerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
-export async function createSupabaseServerClient() {
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export function createSupabaseServerClient(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const cookieStore = await cookies(); // <- pastikan await!
 
   return createServerClient(
